88383438c55f79e30280471f8c2188f65f032d39
Blake Lampkin <blakequake@BlakeQuakes-UFO-157.lan>
2020-07-30 20:34:38 -0700

    got the time working

--- script.js
+++ script.js
@@ -21,16 +21,47 @@ var questions = [
 
 //question index variable keeps track of the current question
 var currentQuestion = 0;
+var userAnswer = [];
+var score = 0;
 
 //start quiz function
-function startQuiz() {
+function startQuiz() 
+{   
     intro.style = "display:none";
     var h2 = document.createElement("h2");
     h2.textContent = questions[currentQuestion].question;
     quizDiv.appendChild(h2)
-    for (var i = 0; i < questions[currentQuestion].answers.length; i++){
+    for (var i = 0; i < questions[currentQuestion].answers.length; i++)
+    {
         var btn = document.createElement("button")
         btn.textContent = questions[currentQuestion].answers[i];
         quizDiv.appendChild(btn)
+        
     }
 }
+//////////////////// TIMER FUNCTION /////////////////////  
+
+var myVar = setInterval(function(){ myTimer() }, 1000);
+var secondlimit = 60;
+
+function myTimer() {
+if(secondlimit == 0)
+{
+    myStopFunction();
+}
+
+document.getElementById("time").innerHTML = '00:' + zeroPad(secondlimit,2);
+secondlimit = secondlimit  - 1;
+
+}
+
+function myStopFunction() {
+    clearInterval(myVar);
+}
+
+function zeroPad(num, places) {
+var zero = places - num.toString().length + 1;
+return Array(+(zero > 0 && zero)).join("0") + num;
+}
+
+
