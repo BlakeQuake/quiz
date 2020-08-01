008fdf01ce44b54c0b47a12837ca9d05df3ca4a1
Blake Lampkin <blakequake@BlakeQuakes-UFO.lan>
2020-07-31 22:54:18 -0700

    clock in sync

--- Safe Script.js
+++ Safe Script.js
@@ -1,7 +1,6 @@
 document.querySelector("#startBtn").addEventListener("click", startQuiz);
 var intro = document.querySelector("#intro");
 var quizDiv = document.querySelector("#quiz");
-var highScores = document.querySelector("#highScores")
 var questions = [
     {
         question: "What color is the sky?",
@@ -25,9 +24,9 @@ var currentQuestion = 0;
 var userAnswer = [];
 var score = 0;
 
-function renderQuestion() 
-{
-    quizDiv.innerHTML=""
+//start quiz function
+function startQuiz() 
+{   
     intro.style = "display:none";
     var h2 = document.createElement("h2");
     h2.textContent = questions[currentQuestion].question;
@@ -36,59 +35,32 @@ function renderQuestion()
     {
         var btn = document.createElement("button")
         btn.textContent = questions[currentQuestion].answers[i];
-        btn.addEventListener("click", validateAnswer);
         quizDiv.appendChild(btn)
-
         
-        function validateAnswer()
-        {
-            if (this.textContent === questions[currentQuestion].correct)
-            {
-                alert ("you got it!")
-                score++
-            }
-            else 
-            {
-                alert ("WRONG!")
-            }
-            currentQuestion++
-            renderQuestion()
-        }
     }
-}
-//////////////// START QUIZ ///////////////////
-function startQuiz() 
-{   
-    renderQuestion()
-
-    var startButton = document.querySelector("#startBtn")
-        startButton.addEventListener("click", startQuiz)
-    
 
 //////////////////// TIMER FUNCTION /////////////////////  
 
         var myVar = setInterval(function(){ myTimer() }, 1000);
         var secondlimit = 60;
 
-        function myTimer() 
-        {
+        function myTimer() {
         if(secondlimit == 0)
-            {
+    {
             myStopFunction();
-            }
+    }
 
         document.getElementById("time").innerHTML = '00:' + zeroPad(secondlimit,2);
         secondlimit = secondlimit  - 1;
-        }
 
-        function myStopFunction() 
-        {
+    }
+
+        function myStopFunction() {
             clearInterval(myVar);
-        }
+    }
 
-        function zeroPad(num, places) 
-        {
+        function zeroPad(num, places) {
         var zero = places - num.toString().length + 1;
         return Array(+(zero > 0 && zero)).join("0") + num;
-        }
+    }
 }
